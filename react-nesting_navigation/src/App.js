a1addf5fadb8f0e950f20f56680a199df125d091
ragnamarie <ragna.steinboemer@gmail.com>
2023-10-25 13:57:22 +0200

    add all components

--- react-nesting_navigation/src/App.js
+++ react-nesting_navigation/src/App.js
@@ -1,35 +1,24 @@
 import "./styles.css";
 
-import avatar from "./img/avatar.jpg";
-import logo from "./img/logo.jpg";
 import { Header } from "./components/Header";
 import { Navigation } from "./components/Navigation";
+import { Link } from "./components/Link";
+import { Avatar } from "./components/Avatar";
+import { Logo } from "./components/Logo";
 
 export default function App() {
   return (
     <>
       <Header>
-        <a href="#">
-          <img className="round-image" src={logo} alt="logo" />
-        </a>
+        <Link href="#">
+          <Logo />
+        </Link>
         <Navigation>
-          <a className="navigation__link" href="#home">
-            Home
-          </a>
-          <a className="navigation__link" href="#about">
-            About
-          </a>
-          <a className="navigation__link" href="#impressum">
-            Impressum
-          </a>
+          <Link href="#home">Home</Link>
+          <Link href="#about">About</Link>
+          <Link href="#impressum">Impressum</Link>
         </Navigation>
-        <button
-          type="button"
-          onClick={() => console.log("I could toggle a profile!")}
-          aria-label="toggle profile"
-        >
-          <img className="round-image" src={avatar} alt="avatar" />
-        </button>
+        <Avatar></Avatar>
       </Header>
       <main>content goes here…</main>
     </>
---
+++ react-nesting_navigation/src/components/Image.js
@@ -0,0 +1,3 @@
+export function Image({ alt, src }) {
+  return <img className="round-image" src={src} alt={alt}></img>;
+}

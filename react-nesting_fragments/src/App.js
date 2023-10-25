98a4d779a98ac724707c174511b9c350a972bc4a
ragnamarie <ragna.steinboemer@gmail.com>
2023-10-25 11:18:55 +0200

    boxes in separate js file

--- react-nesting_fragments/src/App.js
+++ react-nesting_fragments/src/App.js
@@ -1,21 +1,11 @@
 import React from "react";
 import "./styles.css";
-import Box from "./components/Box";
+import { Boxes } from "./components/Boxes";
 
 export default function App() {
   return (
     <main className="flex-container">
       <Boxes />
     </main>
-  );
-}
-
-function Boxes() {
-  return (
-    <>
-      <Box color="#007bff" />
-      <Box color="#fc3" />
-      <Box color="#ff3333" />
-    </>
   );
 }

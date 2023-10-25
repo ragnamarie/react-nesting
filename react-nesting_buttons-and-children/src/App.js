41f146de94570837cf8c5f9ebf99de75cfd6b944
ragnamarie <ragna.steinboemer@gmail.com>
2023-10-25 10:57:41 +0200

    add components

--- react-nesting_buttons-and-children/src/App.js
+++ react-nesting_buttons-and-children/src/App.js
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>hallo</Button>
+      <Button>hello</Button>
+      <Button>salut</Button>
+      <Button>ciao</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }

f47a4c146e865df814968f27f6dc7214290a8ecd
ragnamarie <ragna.steinboemer@gmail.com>
2023-10-25 14:00:30 +0200

    removed one extra a tag

--- react-nesting_navigation/src/components/Logo.js
+++ react-nesting_navigation/src/components/Logo.js
@@ -2,9 +2,5 @@ import { Image } from "./Image";
 import logo from "../img/logo.jpg";
 
 export function Logo() {
-  return (
-    <a>
-      <Image src={logo} alt="logo" />
-    </a>
-  );
+  return <Image src={logo} alt="logo" />;
 }
